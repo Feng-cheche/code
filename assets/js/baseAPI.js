f836dda52f0a824406b4c5c105a55cfedd1d84f7
fengdangye <410665941@qq.com>
2021-03-15 10:04:15 +0800

    首页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -10,4 +10,22 @@ $.ajaxPrefilter(function(options) {
 
     // 拼接对应的环境的服务器地址
     options.url = baseURL + options.url
+
+    // 身份信息优化
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    options.complete = function(res) {
+        console.log(res.responseJSON)
+        console.log(res);
+
+        let obj = res.responseJSON;
+        if (obj.status == 1 && obj.message == '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+$(function() {
+    getUserInof()
+
+
+    //  退出登录
+    $('#btnLogout').on('click', function() {
+        //eg1
+        layer.confirm('您确定要退出吗?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+
+function getUserInof() {
+    $.ajax({
+        url: '/my/userinfo',
+        headers: {
+            // Authorization: localStorage.getItem('token') || ''
+        },
+        success: (res) => {
+            console.log(res);
+            if (res.status != 0) {
+                return layui.layer.msg(res.message, { icon: 5 })
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+
+function renderAvatar(user) {
+    console.log(user)
+        // 渲染用户名，如果有昵称以昵称为准
+    let name = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    // 判断图片头像是否存在
+    if (user.user_pic == null) {
+        // 隐藏图片头像，渲染文字头像
+        $('.layui-nav-img').hide();
+        $('.text-avatar').show().html(name[0].toUpperCase());
+    } else {
+        // 渲染图片头像，隐藏文字头像
+        $('.layui-nav-img').show().attr('src', user.user_pic);
+        $('.text-avatar').hide()
+    }
+}
